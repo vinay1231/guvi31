e8e0b2d090aa830ceaa6d87aff258fbdfff34d1e
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-08-17 12:25:50 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class GUVI
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int m=sc.nextInt();
+
+          int avg=m+n;
+
+          System.out.println(avg);
+
+	}
+
+}
