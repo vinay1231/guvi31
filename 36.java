b310fd83db0e4d8f7cb718b1bf6ab52e3e246fc0
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-08-17 11:38:21 +0530

    Create 36.java

---
+++ 36.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		Scanner sc=new Scanner(System.in);
+
+		int m=sc.nextInt();
+
+		int n=sc.nextInt();
+
+		int temp;
+
+		temp=m;
+
+		m=n;
+
+		n=temp;
+
+		System.out.print(m+" "+n);
+
+	}
+
+}
