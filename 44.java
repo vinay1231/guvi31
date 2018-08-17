d421e658bed59ccaf31fa72366fa87ae9b1c9293
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-08-17 12:17:24 +0530

    Create 44.java

---
+++ 44.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		Scanner sc=new Scanner(System.in);
+
+		int n=sc.nextInt();
+
+		if(n>=1&&n<=10)
+
+		System.out.println("yes");
+
+		else
+
+		 System.out.println("no");
+
+	}
+
+}
